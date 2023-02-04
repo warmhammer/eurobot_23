9799a3f5c505b46cadb855a1fbfeb6e9c925d9d1
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-02-04 14:40:51 +0300

    refactoring and bug fixing

--- catkin_ws/src/camera_calibration/scripts/calibrate_camera.py
+++ catkin_ws/src/camera_calibration/scripts/calibrate_camera.py
@@ -1,103 +1,107 @@
+import argparse
 import os
 import cv2
 import glob
 import numpy
-import sys
 from os import path
 from os.path import abspath
 
-
 this_dir = path.dirname(abspath(__file__))
 
-params = {'chessboardCornersCountX': 9, 'chessboardCornersCountY': 6,
-          'sizeOfSquareSide': 24, 'frameWidth': 640, 'frameHeight': 480, 'imagesPath': f'{this_dir}/../images'}
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        args = sys.argv[1:]
+def configure_arg_parser():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-cx", "--chessboardCornersCountX", type=int, required=True,
+                            help="Number of internal corners of the calibration chessboard horizontally")
+    arg_parser.add_argument("-cy", "--chessboardCornersCountY", type=int, required=True,
+                            help="Number of internal corners of the calibration chessboard vertically")
+    arg_parser.add_argument("-ss", "--sizeOfSquareSide", type=int, required=True,
+                            help="size of the side of the square of the calibration chessboard in mm")
+    arg_parser.add_argument("-fw", "--frameWidth", type=int, required=True,
+                            help="Frame width of the images")
+    arg_parser.add_argument("-fh", "--frameHeight", type=int, required=True,
+                            help="Frame height of the images")
+    arg_parser.add_argument("-ip", "--imagesPath", type=str, default=f'{this_dir}/../images',
+                            help="Path to images that will be used for camera calibration. Default path: "
+                                 f"'{abspath(f'{this_dir}/../images')}")
+    arg_parser.add_argument("-op", "--outputPath", type=str, default=f'{this_dir}/../camera_info',
+                            help="Path - where the calibration results will be saved. Default: "
+                                 f"'{abspath(f'{this_dir}/../camera_info')}'\nThe script creates folder if it doesn't "
+                                 "exist, deletes all existing files in it")
+    return arg_parser
 
-        for i in params.keys():
-            if args.__contains__(i) and args.index(i) + 1 < len(args) and args[args.index(i) + 1].isdigit():
-                params[i] = int(args[args.index(i) + 1])
 
-
-################ FIND CHESSBOARD CORNERS - OBJECT POINTS AND IMAGE POINTS #############################
-
-    chessboardSize = (params['chessboardCornersCountX'], params['chessboardCornersCountY'])
-    frameSize = (params['frameWidth'], params['frameHeight'])
+def find_chessboard_corners():
+    chessboard_size = (args['chessboardCornersCountX'], args['chessboardCornersCountY'])
 
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = numpy.zeros((chessboardSize[0] * chessboardSize[1], 3), numpy.float32)
-    objp[:, :2] = numpy.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
+    objp = numpy.zeros((chessboard_size[0] * chessboard_size[1], 3), numpy.float32)
+    objp[:, :2] = numpy.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1, 2)
 
-    size_of_chessboard_squares_mm = params['sizeOfSquareSide']
+    size_of_chessboard_squares_mm = args['sizeOfSquareSide']
     objp = objp * size_of_chessboard_squares_mm
 
-
     # Arrays to store object points and image points from all the images.
-    objPoints = []  # 3d point in real world space
-    imgPoints = []  # 2d points in image plane.
-
-
-
-    if not path.exists(params['imagesPath']):
-        print(f"path to images '{params['imagesPath']}' does'nt exists")
-        exit(-1)
-
-    images = glob.glob(f"{params['imagesPath']}/*.png")
+    obj_points = []  # 3d point in real world space
+    img_points = []  # 2d points in image plane.
+    images = glob.glob(f"{args['imagesPath']}/*.png")
     if len(images) < 1:
-        print(f"there is no .png files in {params['imagesPath']}")
-        exit(-1)
-
+        raise FileNotFoundError(f"There is no any .png files in '{path.abspath(args['imagesPath'])}'")
     for image in images:
 
         img = cv2.imread(image)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, chessboardSize, None)
+        ret_val, corners = cv2.findChessboardCorners(gray, chessboard_size, None)
 
         # If found, add object points, image points (after refining them)
-        if ret:
-            objPoints.append(objp)
+        if ret_val:
+            obj_points.append(objp)
             corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-            imgPoints.append(corners)
+            img_points.append(corners)
 
             # Draw and display the corners
-            cv2.drawChessboardCorners(img, chessboardSize, corners2, ret)
+            cv2.drawChessboardCorners(img, chessboard_size, corners2, ret_val)
             cv2.imshow('img', img)
             cv2.waitKey(300)
-
     cv2.destroyAllWindows()
+    return obj_points, img_points
 
 
-############## CALIBRATION #######################################################
+def save_to_file():
+    if not path.isdir(args['outputPath']):
+        os.makedirs(args['outputPath'])
+    else:
+        for f in os.listdir(args['outputPath']):
+            os.remove(os.path.join(args['outputPath'], f))
+    file_path = f"{args['outputPath']}/camera_info.npz"
+    numpy.savez(file_path, cameraMatrix=camMtx, distCoeffs=dist)
 
-    ret, camMtx, dist, rVectors, tVectors = cv2.calibrateCamera(objPoints, imgPoints, frameSize, None, None)
 
-    if ret:
+if __name__ == "__main__":
+    ap = configure_arg_parser()
+    args = vars(ap.parse_args())
+    if not path.isdir(args['imagesPath']):
+        raise FileNotFoundError(f"Path to images '{abspath(args['imagesPath'])}' doesn't exists")
 
-        dir = f'{this_dir}/../camera_info'
-        if not path.exists(dir):
-            os.makedirs(dir)
-        else:
-            for f in os.listdir(dir):
-                os.remove(os.path.join(dir, f))
+    frameSize = (args['frameWidth'], args['frameHeight'])
 
-        numpy.savez(f'{dir}/camera_info.npz', cameraMatrix=camMtx, distCoeff=dist, rVectors=rVectors, tVectors=tVectors)
+    objPoints, imgPoints = find_chessboard_corners()
 
-        print('calibration was successful\nresult:')
-        print('intrinsicMatrix:')
-        print(camMtx)
-        print('\ndist:')
-        print(dist)
-        print('\nrVectors:')
-        print(rVectors)
-        print('\ntVectors:')
-        print(tVectors)
+    retVal, camMtx, dist, _, _ = cv2.calibrateCamera(objPoints, imgPoints, frameSize, None, None)
+
+    if retVal:
+        save_to_file()
+
+        print('calibration was successful',
+              'camera info:',
+              'cameraMatrix:', camMtx,
+              'distortionCoeffs:', dist,
+              f'results were saved in {abspath(args["outputPath"])}/camera_info.npz',
+              sep='\n\n')
     else:
         print('calibration failed')
--- catkin_ws/src/camera_calibration/scripts/take_images.py
+++ catkin_ws/src/camera_calibration/scripts/take_images.py
@@ -1,56 +1,66 @@
+import argparse
 import cv2
-import sys
 import os
 from os import path
 from os.path import abspath
 
 
-params = {'cameraIndex': 0, 'frameWidth': 640, 'frameHeight': 480}
+this_dir = path.dirname(abspath(__file__))
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        args = sys.argv[1:]
 
-        for i in params.keys():
-            if args.__contains__(i) and args.index(i) + 1 < len(args) and args[args.index(i) + 1].isdigit():
-                params[i] = int(args[args.index(i) + 1])
+def configure_arg_parser() -> argparse.ArgumentParser:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-ci", "--cameraIndex", type=int, default=0,
+                            help="Camera index in system. Default: 0")
+    arg_parser.add_argument("-fw", "--frameWidth", type=int, required=True,
+                            help="Frame width")
+    arg_parser.add_argument("-fh", "--frameHeight", type=int, required=True,
+                            help="Frame height")
+    arg_parser.add_argument("-op", "--outputPath", type=str, default=f'{this_dir}/../images',
+                            help=f"Path - where the images will be saved. Default: '{abspath(f'{this_dir}/../images')}'"
+                                 "\nThe script creates folder if it doesn't exist, deletes all existing files in it")
+    return arg_parser
 
-    cap = cv2.VideoCapture(params['cameraIndex'])
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, params['frameWidth'])
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, params['frameHeight'])
 
+def configure_video_capturer():
+    cap = cv2.VideoCapture(args['cameraIndex'])
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, args['frameWidth'])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args['frameHeight'])
     if not cap.isOpened():
-        print('Can not open camera by index')
-        exit(0)
+        raise ValueError(f"Can not open camera by index {args['cameraIndex']}")
+    return cap
 
 
-    this_dir = path.dirname(abspath(__file__))
-    dir = f'{this_dir}/../images'
-    if not path.exists(dir):
-        os.makedirs(dir)
-    else:
-        for f in os.listdir(dir):
-            os.remove(os.path.join(dir, f))
-
-
+def take_images():
     count = 1
-    while cap.isOpened():
-        ret, frame = cap.read()
+    while capturer.isOpened():
+        ret_val, frame = capturer.read()
 
-        if ret:
+        if ret_val:
             cv2.imshow('img', frame)
             key = cv2.waitKey(1)
 
             # save frame to file when pressing spacebar or 's' key
             if key == ord(' ') or key == ord('s'):
-
-                
-
-                cv2.imwrite(f"{this_dir}/../images/img{count}.png", frame)
+                file_path = f"{args['outputPath']}/img{count}.png"
+                cv2.imwrite(file_path, frame)
                 count += 1
-
+            # exit when pressing 'q' or Esc
             elif key == ord('q') or key == 27:
                 break
 
+
+if __name__ == "__main__":
+    ap = configure_arg_parser()
+    args = vars(ap.parse_args())
+    if not path.isdir(args['outputPath']):
+        os.makedirs(args['outputPath'])
+    else:
+        for f in os.listdir(args['outputPath']):
+            os.remove(os.path.join(args['outputPath'], f))
+
+    capturer = configure_video_capturer()
+    take_images()
+    print(f"Taken images were saved in {abspath(args['outputPath'])}")
     cv2.destroyAllWindows()
-    cap.release()
+    capturer.release()
