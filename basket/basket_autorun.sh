f3a74a527bfc4fff0193fc01b0485daa41779818
Maxim Popov <max.popov.02@ya.ru>
2023-03-08 20:29:18 +0300

    basket_autorun: chmod and script path

--- basket/basket_autorun.sh
+++ basket/basket_autorun.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export DISLPAY=:0 
-x-terminal-emulator --geometry="800x480" -e "/home/rpizero/ws/scripts/box_script.py"
+x-terminal-emulator --geometry="800x480" -e "/home/rpizero/ws/eurobot_23/basket/basket_script.py"
