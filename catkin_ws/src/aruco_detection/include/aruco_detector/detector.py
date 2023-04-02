a9d0eafa9b3bc5e85bd8aab19c0ac2584b18a4db
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-04-02 22:57:16 +0300

    modified detector.py

--- catkin_ws/src/aruco_detection/include/aruco_detector/detector.py
+++ catkin_ws/src/aruco_detection/include/aruco_detector/detector.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 from cv2 import aruco
 import math
-from .object_pose import ObjectPose
+from typing import Dict, Tuple, Any, Optional
+
 
 class Detector:
 
@@ -17,51 +18,35 @@ class Detector:
         self.__arucoDictType = aruco_dict_type
 
 
-    def __camera_to_world_coords(self, rotation_vector, translation_vector):
-        rotation_matrix, _ = cv2.Rodrigues(rotation_vector)
-        x, y, z = np.dot(-(np.linalg.inv(rotation_matrix)),
-                                        translation_vector)
-        roll, pitch, yaw = self.__rotation_matrix_to_euler_angles(rotation_matrix)
-        pose = ObjectPose(x, y, z, roll, pitch, yaw)
+    def detect_markers(self, frame) -> Tuple[Dict[int, Any], Optional[Any]]:
 
-        return pose
-    
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        aruco_dict = aruco.Dictionary_get(self.__arucoDictType)
+        parameters = aruco.DetectorParameters_create()
+        corners, ids, _ = aruco.detectMarkers(gray_frame, aruco_dict, parameters=parameters,
+                                              cameraMatrix=self.__cameraMatrix, distCoeff=self.__distCoeff)
 
-    def __rotation_matrix_to_euler_angles(self, rotation_mtx):
-        sy = math.sqrt(rotation_mtx[0, 0] * rotation_mtx[0, 0] + rotation_mtx[1, 0] * rotation_mtx[1, 0])
+        rotation_matricies = {}
 
-        singular = sy < 1e-2
+        if ids is not None:
 
-        if not singular:
-            roll = math.atan2(rotation_mtx[2, 1], rotation_mtx[2, 2])
-            pitch = math.atan2(-rotation_mtx[2, 0], sy)
-            yaw = math.atan2(rotation_mtx[1, 0], rotation_mtx[0, 0])
-        else:
-            roll = math.atan2(-rotation_mtx[1, 2], rotation_mtx[1, 1])
-            pitch = math.atan2(-rotation_mtx[2, 0], sy)
-            yaw = 0
+            for i in range(len(ids)):
+                rotation_vector, translation_vector, _ = aruco.estimatePoseSingleMarkers(corners[i], self._markerSize,
+                                                                                         self.__cameraMatrix,
+                                                                                         self.__distCoeff)
+                rotation_matrix, _ = cv2.Rodrigues(rotation_vector[0][0])
+                rotation_matrix[2, :2] = rotation_matrix[2, :2] * -1
+                rotation_matrix[:2, 2] = rotation_matrix[:2, 2] * -1
+                translation_vector = translation_vector[0][0]
+                translation_vector[:2] = translation_vector[:2] * -1
 
-        return roll, pitch, yaw
+                rotation_matrix_4x4 = np.eye(4)
+                rotation_matrix_4x4[:3, :3] = rotation_matrix
+                rotation_matrix_4x4[:3, 3] = translation_vector
 
+                id = ids[i][0]
+                rotation_matricies[id] = rotation_matrix_4x4
 
-    def detect_markers(self, frame): 
-            
-            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            aruco_dict = aruco.Dictionary_get(self.__arucoDictType)
-            parameters = aruco.DetectorParameters_create()
-            corners, ids, _ = aruco.detectMarkers(gray_frame, aruco_dict, parameters=parameters, 
-                                                  cameraMatrix=self.__cameraMatrix, distCoeff=self.__distCoeff)
+            frame = aruco.drawDetectedMarkers(frame, corners)
 
-            markers_poses: dict[int, ObjectPose] = {}
-
-            if(ids is not None):
-
-                for i in range(len(ids)):
-                    rotation_vector, translation_vector, _ = aruco.estimatePoseSingleMarkers(corners[i], self._markerSize,
-                                                                            self.__cameraMatrix, self.__distCoeff)
-                    id = ids[i][0]
-                    markers_poses[id] = self.__camera_to_world_coords(rotation_vector[0][0], translation_vector[0][0])
-
-                frame = aruco.drawDetectedMarkers(frame, corners)
-
-            return markers_poses, frame
+        return rotation_matricies, frame
