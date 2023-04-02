4d68c74627b2879e793f90fa38ff8b551ce4735e
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-04-02 22:59:14 +0300

    modified test.py

--- catkin_ws/src/aruco_detection/tests/test.py
+++ catkin_ws/src/aruco_detection/tests/test.py
@@ -1,15 +1,26 @@
+import cv2
+import rospy
+from tf import TransformBroadcaster
+from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_matrix
+from tf.transformations import translation_from_matrix
 import sys
 import os
 from os.path import abspath
 from os import path
 sys.path.append(abspath(path.dirname(__file__) + '/../'))
 from include.aruco_detector import Detector
-from include.aruco_detector import ObjectPose
-import cv2
-import math
 
 
-params = {'cameraIndex': 0, 'frameWidth': 320, 'frameHeight': 240}
+rospy.init_node("aruco_test")
+tb = TransformBroadcaster()
+
+this_dir = os.path.dirname(__file__)
+camera_info_path = abspath(this_dir + '/../../camera_calibration/camera_info/camera_info.npz')
+detector = Detector(0.1, cv2.aruco.DICT_4X4_100, camera_info_path)
+
+
+params = {'cameraIndex': 0, 'frameWidth': 640, 'frameHeight': 480}
 
 cap = cv2.VideoCapture(params['cameraIndex'])
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, params['frameWidth'])
@@ -19,26 +30,25 @@ if not cap.isOpened():
     print('Can not open camera by index')
     exit(0)
 
-this_dir = os.path.dirname(__file__)
-camera_info_path = abspath(this_dir + '/../../camera_calibration/camera_info/camera_info.npz')
-detector = Detector(0.1, cv2.aruco.DICT_4X4_100, camera_info_path)
 
-while cap.isOpened():
+
+rate = rospy.Rate(30)
+
+while cap.isOpened() and not rospy.is_shutdown():
     ret, frame = cap.read()
 
     if ret:
         
         key = cv2.waitKey(1)
 
-        markers_poses, res_frame = detector.detect_markers(frame)
+        rotation_matricies, res_frame = detector.detect_markers(frame)
 
-        print(f'=== markers poses ===')
-        for id, pose in markers_poses.items():
-            pose.Roll = math.degrees(pose.Roll)
-            pose.Pitch = math.degrees(pose.Pitch)
-            pose.Yaw = math.degrees(pose.Yaw)
-            print('\nid: %d' % id, '\nx:\t%0.3f \ny:\t%0.3f \nz:\t%0.3f' %(pose.X, pose.Y, pose.Z),
-                '\nroll:\t%d, \npitch:\t%d, \nyaw:\t%d\n' %(pose.Roll, pose.Pitch, pose.Yaw), sep='\n')
+        for id, r_mtx in rotation_matricies.items():
+            tb.sendTransform(translation_from_matrix(r_mtx), 
+                             quaternion_from_matrix(r_mtx), 
+                             rospy.Time.now(), f"marker_id_{id}", "/camera")
+
+            rate.sleep()
 
         cv2.imshow('img', res_frame)
         if key == ord('q') or key == 27:
