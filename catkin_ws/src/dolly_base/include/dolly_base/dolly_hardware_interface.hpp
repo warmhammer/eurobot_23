0ec428696439ba2c6b86f075302e30714e3779ca
Maxim Popov <max.popov.02@ya.ru>
2023-04-03 21:57:42 +0300

    dolly_hw_interface: undo changes, little beauty

--- catkin_ws/src/dolly_base/include/dolly_base/dolly_hardware_interface.hpp
+++ catkin_ws/src/dolly_base/include/dolly_base/dolly_hardware_interface.hpp
@@ -1,5 +1,5 @@
-#ifndef DOLLY_HW
-#define DOLLY_HW
+#ifndef DOLLY_HARDWARE_INTERFACE
+#define DOLLY_HARDWARE_INTERFACE
 
 #include <ros/ros.h>
 #include <ros/console.h>
@@ -11,9 +11,6 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/Float64.h>
 
-#include <std_msgs/Float32MultiArray.h>
-#include <std_msgs/MultiArrayDimension.h>
-
 #include <string>
 #include <vector>
 
@@ -22,23 +19,12 @@ namespace dolly_hw {
         const std::string name;
 		double pos = 0;
         double prev_pos = 0;
-		// double pos_offset = 0;
 		double vel = 0;
 		double eff = 0;
 		double cmd = 0;
 
         Joint() = delete;
         Joint(const std::string& name) : name(name) {}
-
-        // void print() {
-        //     ROS_INFO_STREAM (
-        //         "name: " << name << "\n"
-        //         << "pos: " << pos << "\n"
-        //         << "vel: " << vel << "\n"
-        //         << "eff: " << eff << "\n"
-        //         << "cmd: " << cmd << "\n"
-        //     );
-        // }
 	};
 
     class DollyHW : public hardware_interface::RobotHW {
@@ -61,8 +47,6 @@ namespace dolly_hw {
             Joint _left_wheel;
             Joint _right_wheel;
 
-            std_msgs::Float32MultiArray servo_cmd;
-
             ros::NodeHandle _root_node;
             ros::NodeHandle _robot_hw_node;
 
@@ -70,7 +54,6 @@ namespace dolly_hw {
             ros::Subscriber _right_wheel_angle_sub;
             ros::Publisher _left_wheel_cmd_vel_pub;
             ros::Publisher _right_wheel_cmd_vel_pub;
-            ros::Publisher _servo_cmd_pub;
     };
 
     DollyHW::DollyHW(ros::NodeHandle& root_node, ros::NodeHandle& robot_hw_node) 
@@ -81,20 +64,11 @@ namespace dolly_hw {
 
         _registerJoints();
 
-        servo_cmd.layout.dim.push_back(std_msgs::MultiArrayDimension());
-        servo_cmd.layout.dim[0].size = 3;
-        servo_cmd.layout.dim[0].stride = 1;
-        servo_cmd.layout.dim[0].label = "servo_cmd";
-        //servo_cmd.data = {50, 50, 50};
-
         _left_wheel_angle_sub = _root_node.subscribe("/dolly/left_wheel/angle32", 1, &DollyHW::_leftWheelCallback, this);
         _right_wheel_angle_sub = _root_node.subscribe("/dolly/right_wheel/angle32", 1, &DollyHW::_rightWheelCallback, this);
 
         _left_wheel_cmd_vel_pub = _root_node.advertise<std_msgs::Float64>("/dolly/left_wheel/cmd_vel64", 1);
         _right_wheel_cmd_vel_pub = _root_node.advertise<std_msgs::Float64>("/dolly/right_wheel/cmd_vel64", 1);
-
-        _servo_cmd_pub = _root_node.advertise<std_msgs::Float32MultiArray>("/servo_cmd_topic",1);
-        
     }
 
     void DollyHW::_registerJoints() {
@@ -121,11 +95,6 @@ namespace dolly_hw {
     void DollyHW::read(const ros::Duration& delta) {
         std::vector<Joint*> joints = {&_left_wheel, &_right_wheel};
 
-        // for(auto& joint : joints) {
-        //     joint->pos += joint->cmd * delta.toSec();
-        //     joint->vel = joint->cmd;
-        // }
-
         for(auto& joint : joints) {
             joint->vel = (joint->pos - joint->prev_pos) / delta.toSec();
             joint->prev_pos = joint->pos;
@@ -141,8 +110,6 @@ namespace dolly_hw {
 
         _left_wheel_cmd_vel_pub.publish(left_cmd_vel);
         _right_wheel_cmd_vel_pub.publish(right_cmd_vel);
-
-        //_servo_cmd_pub.publish(servo_cmd);
     }
 
     void DollyHW::_leftWheelCallback(const std_msgs::Float32& angle) {
@@ -154,4 +121,4 @@ namespace dolly_hw {
     }
 }
 
-#endif // DOLLY_HW
+#endif // DOLLY_HARDWARE_INTERFACE
