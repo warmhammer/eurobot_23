3ace429bd477e209496409e40e30b03cd1770f46
ArcherBTD <valera.valera3945@gmail.com>
2023-04-18 14:36:10 +0300

    remove interrupt pin from configure

--- Eurobot2023/Core/Src/main.cpp
+++ Eurobot2023/Core/Src/main.cpp
@@ -847,10 +847,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(ENA_GPIO_Port, &GPIO_InitStruct);
 
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
-
 }
 
 /* USER CODE BEGIN 4 */
