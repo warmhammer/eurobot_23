5155a57e4c5d70067c294f2daf95292e585dd1fa
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-04-27 02:12:32 +0300

    refactoring camera_pose_node

--- catkin_ws/src/aruco_detection/src/camera_pose_node.py
+++ catkin_ws/src/aruco_detection/src/camera_pose_node.py
@@ -1,7 +1,7 @@
 import rospy
 import rospkg
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge
 import numpy as np
 import cv2
 import sys
@@ -14,106 +14,92 @@ from scrips.camera_pose_common import get_table_pose_and_rotation_matrix
 from scrips.configs_provider import ConfigsProvider
 
 
-recived_img = None
+class ImageReceiver:
+    def __init__(self, configs):
+        self.__received_img = None
+        self._configs = configs
+
+    def receive_from_camera(self):
+        cap_props = self._configs.get_capturer_props()
+        self.__received_img = self.__get_frame_from_camera()
+        return self.__received_img
+
+    def img_callback(self, img_msg, cv_bridge):
+        if self.__received_img is None:
+            cv_image = cv_bridge.imgmsg_to_cv2(img_msg, "bgr8")
+            self.__received_img = cv_image
+
+    def receive_from_topic(self, wait_time):
+        self.__received_img = None
+        start_time = time.time()
+        rate = rospy.Rate(30)
+        while self.__received_img is None and time.time() - start_time < wait_time:
+            rate.sleep()
+        return self.__received_img
+
+    def __get_frame_from_camera(self):
+        try:
+            ci, fw, fh = self._configs.get_capturer_props()
+            cap = cv2.VideoCapture(ci)
+            cap.set(cv2.CAP_PROP_FRAME_WIDTH, fw)
+            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, fh)
+            if not cap.isOpened():
+                raise cv2.error()
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if ret == True:
+                    cap.release()
+                    return frame
+        except cv2.error as e:
+            rospy.logerr(f"Can not open camera. Exception text:\n{e}")
+        finally:
+            rospy.set_param('camera_pose_ok', False)
+            exit()
 
 
-def get_frame_from_camera(camera_index, frame_width, frame_height):
-    try:
-        cap = cv2.VideoCapture(camera_index)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
-        if not cap.isOpened():
-            raise cv2.error()
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if ret == True:
-                cap.release()
-                return frame
+def determine_and_broadcast_camera_pose(table_pose, table_rmtx, t_broadcaster):
+    r_table_to_cam = tft.inverse_matrix(table_rmtx)
+    T_table_to_world = np.dot(tft.translation_matrix(table_pose[:3]),
+                              tft.euler_matrix(*table_pose[3:]))
+    T_cam_world = np.dot(T_table_to_world, r_table_to_cam)
+    rospy.set_param('camera_pose_ok', True)
+    rate = rospy.Rate(30)
+    while not rospy.is_shutdown():
+        t_broadcaster.sendTransform(tft.translation_from_matrix(T_cam_world),
+                                    tft.quaternion_from_matrix(T_cam_world),
+                                    rospy.Time.now(), "camera", "/world")
+        rate.sleep()
 
-    except cv2.error as e:
-        rospy.logerr(f"Can not open camera. Exception text:\n{e}")
-    finally:
-        rospy.set_param('camera_pose_ok', False)
-        exit()
-
-def image_callback(img_msg, cv_bridge):
-    try:
-        cv_image = cv_bridge.imgmsg_to_cv2(img_msg, "bgr8")
-
-    except CvBridgeError as e:
-        rospy.logerr(f"CvBridge Error. Exception text:\n{e}")
-
-    global recived_img
-    if recived_img is None:
-        recived_img = cv_image
 
 def main():
     rospy.init_node("camera_pose_node")
     configs = ConfigsProvider()
     cv_bridge = CvBridge()
     tb = TransformBroadcaster()
-    global recived_img
-    use_camera = rospy.get_param('~use_camera', 'true')
     rate = rospy.Rate(30)
-
-    camera_index, frame_width, frame_height = configs.get_capturer_props()
-    if use_camera == True:
-        recived_img = get_frame_from_camera(camera_index, frame_width, frame_height)
-
-    else:
-        img_sub = rospy.Subscriber("/my_robot/camera1/image_raw", Image, lambda msg: image_callback(msg, cv_bridge))
-        start_time = time.time()
-
-        while recived_img is None and time.time() - start_time < 1:
-            rate.sleep()
-
-    frame = recived_img
-
-    camera_matrix, dist_coeffs = configs.get_camera_info()
+    receiver = ImageReceiver(configs)
+    use_camera = rospy.get_param('~use_camera', True)
+    if not use_camera:
+        topic_name = rospy.get_param("~receiving_img_topic", "/my_robot/camera1/image_raw")
+        rospy.Subscriber(topic_name, Image, lambda msg: receiver.img_callback(msg, cv_bridge))
+    camera_info = configs.get_camera_info()
     static_markers = configs.get_markers_config()["static_markers"]
-    for i in range(3):
-        ret, table_pose, rmtx = get_table_pose_and_rotation_matrix(frame, static_markers, camera_matrix, dist_coeffs)
-
-        if ret == True:
-            break
-
-        if use_camera == True:
-            frame = get_frame_from_camera(camera_index, frame_width, frame_height)
-
+    for _ in range(3):
+        if use_camera:
+            frame = receiver.receive_from_camera()
         else:
-            recived_img = None
-            start_time = time.time()
-
-            while recived_img is None and time.time() - start_time < 1:
-                rate.sleep()
-
-            if recived_img is None:
-                break
-
-            frame = recived_img
+            wait_time = 1  # seconds
+            frame = receiver.receive_from_topic(wait_time)
+        success, table_pose, rmtx = get_table_pose_and_rotation_matrix(frame, static_markers, *camera_info)
+        if success:
+            break
         rate.sleep()
-
-    if ret != True:
+    if not success:
         rospy.logerr("Camera position was not found")
         rospy.set_param('camera_pose_ok', False)
         exit()
+    determine_and_broadcast_camera_pose(table_pose, rmtx, tb)
 
-    img_sub.unregister()
-
-    r_table_to_cam = tft.inverse_matrix(rmtx)
-
-    T_table_to_world = np.dot(tft.translation_matrix(table_pose[:3]), 
-                            tft.euler_matrix(table_pose[3], table_pose[4], table_pose[5]))
-
-    T_cam_world = np.dot(T_table_to_world, r_table_to_cam)
-
-    rospy.set_param('camera_pose_ok', True)
-
-    while not rospy.is_shutdown():
-        tb.sendTransform(tft.translation_from_matrix(T_cam_world), 
-                        tft.quaternion_from_matrix(T_cam_world), 
-                        rospy.Time.now(), "camera", "/world")
-        rate.sleep()
 
 if __name__ == "__main__":
     main()
