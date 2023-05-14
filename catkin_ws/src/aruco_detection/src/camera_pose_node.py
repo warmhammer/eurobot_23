95ce218b75d445d87d96b7ea43b3d760397573ab
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-05-15 01:07:48 +0300

    modified camera_pose_node.py

--- catkin_ws/src/aruco_detection/src/camera_pose_node.py
+++ catkin_ws/src/aruco_detection/src/camera_pose_node.py
@@ -2,7 +2,6 @@ import rospy
 import rospkg
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-import numpy as np
 import cv2
 import sys
 import time
@@ -19,11 +18,6 @@ class ImageReceiver:
         self.__received_img = None
         self._configs = configs
 
-    def receive_from_camera(self):
-        cap_props = self._configs.get_capturer_props()
-        self.__received_img = self.__get_frame_from_camera()
-        return self.__received_img
-
     def img_callback(self, img_msg, cv_bridge):
         if self.__received_img is None:
             cv_image = cv_bridge.imgmsg_to_cv2(img_msg, "bgr8")
@@ -37,7 +31,7 @@ class ImageReceiver:
             rate.sleep()
         return self.__received_img
 
-    def __get_frame_from_camera(self):
+    def receive_from_camera(self):
         try:
             ci, fw, fh = self._configs.get_capturer_props()
             cap = cv2.VideoCapture(ci)
@@ -49,7 +43,8 @@ class ImageReceiver:
                 ret, frame = cap.read()
                 if ret == True:
                     cap.release()
-                    return frame
+                    self.__received_img = frame
+                    return self.__received_img
         except cv2.error as e:
             rospy.logerr(f"Can not open camera. Exception text:\n{e}")
         finally:
@@ -58,11 +53,12 @@ class ImageReceiver:
 
 
 def determine_and_broadcast_camera_pose(table_pose, table_rmtx, t_broadcaster):
+    T_table_to_world = tft.euler_matrix(*table_pose[3:])
+    T_table_to_world[:3, 3] = table_pose[:3]
     r_table_to_cam = tft.inverse_matrix(table_rmtx)
-    T_table_to_world = np.dot(tft.translation_matrix(table_pose[:3]),
-                              tft.euler_matrix(*table_pose[3:]))
-    T_cam_world = np.dot(T_table_to_world, r_table_to_cam)
+    T_cam_world = T_table_to_world @ r_table_to_cam
     rospy.set_param('camera_pose_ok', True)
+    rospy.set_param('T_camera_to_world', T_cam_world.tolist())
     rate = rospy.Rate(30)
     while not rospy.is_shutdown():
         t_broadcaster.sendTransform(tft.translation_from_matrix(T_cam_world),
@@ -76,7 +72,6 @@ def main():
     configs = ConfigsProvider()
     cv_bridge = CvBridge()
     tb = TransformBroadcaster()
-    rate = rospy.Rate(30)
     receiver = ImageReceiver(configs)
     use_camera = rospy.get_param('~use_camera', True)
     if not use_camera:
@@ -84,6 +79,7 @@ def main():
         rospy.Subscriber(topic_name, Image, lambda msg: receiver.img_callback(msg, cv_bridge))
     camera_info = configs.get_camera_info()
     static_markers = configs.get_markers_config()["static_markers"]
+    rate = rospy.Rate(2)  # 2 hz
     for _ in range(3):
         if use_camera:
             frame = receiver.receive_from_camera()
